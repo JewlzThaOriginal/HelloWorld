f6f13ff25a897015f90afbc5c2c7873a8ee26e6a
Aiden Martinez <Angrybull2007@gmail.com>
2022-02-01 14:22:57 -0700

    Dubble

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Skillet: UILabel!
     override func viewDidLoad() {
+        
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Skillet.text = "
+        
     }
 
 
